b5b1b8c6b35cd739ef335308fd7d3e0bf9e88f7e
Quentin DEL MISTRO <quentin.delmistro@epsi.fr>
2017-04-11 13:48:41 +0200

    correction bug

--- PDF-Merge-Application/src/pdf/merge/application/DAL/PropertiesModel.java
+++ PDF-Merge-Application/src/pdf/merge/application/DAL/PropertiesModel.java
@@ -22,6 +22,23 @@ public class PropertiesModel {
     private File myConfig = new File(fileName);
     private FileOutputStream output = null;
     
+    public void setProperties(String filePath, Boolean headerIsActivated, Boolean footerIsActivated, String headerContent, String footerContent) {
+        try {
+            InputStream file = new FileInputStream(myConfig);
+            output = new FileOutputStream(myConfig, false);
+            prop.load(file);
+            
+            prop.setProperty("generatedPDFFilePath", filePath);
+            prop.setProperty("isTextOnTopActivated", headerIsActivated.toString());
+            prop.setProperty("isTextOnBottomActivated", footerIsActivated.toString());
+            prop.setProperty("headerContent", headerContent);
+            prop.setProperty("footerContent", footerContent);
+            prop.store(output, null);
+        } catch (IOException e) {
+            System.out.println("Erreur : " + e);
+        }
+    }
+    
     public String getFilePath() {
         try {
             InputStream file = new FileInputStream(myConfig);
@@ -33,15 +50,25 @@ public class PropertiesModel {
         }
     }
     
-    public void setFilePath(String value) {
+    public String getHeaderIsActivated() {
         try {
             InputStream file = new FileInputStream(myConfig);
-            output = new FileOutputStream(myConfig, false);
             prop.load(file);
-            prop.setProperty("generatedPDFFilePath", value);
-            prop.store(output, null);
+            return prop.getProperty("isTextOnTopActivated");
         } catch (IOException e) {
             System.out.println("Erreur : " + e);
+            return null;
+        }
+    }
+    
+    public String getFooterIsActivated() {
+        try {
+            InputStream file = new FileInputStream(myConfig);
+            prop.load(file);
+            return prop.getProperty("isTextOnBottomActivated");
+        } catch (IOException e) {
+            System.out.println("Erreur : " + e);
+            return null;
         }
     }
     
@@ -56,18 +83,6 @@ public class PropertiesModel {
         }
     }
     
-    public void setHeaderContent(String value) {
-        try {
-            InputStream file = new FileInputStream(myConfig);
-            output = new FileOutputStream(myConfig, false);
-            prop.load(file);
-            prop.setProperty("headerContent", value);
-            prop.store(output, null);
-        } catch (IOException e) {
-            System.out.println("Erreur : " + e);
-        }
-    }
-    
     public String getFooterContent() {
         try {
             InputStream file = new FileInputStream(myConfig);
@@ -76,18 +91,6 @@ public class PropertiesModel {
         } catch (IOException e) {
             System.out.println("Erreur : " + e);
             return null;
-        }
-    }
-    
-    public void setFooterContent(String value) {
-        try {
-            InputStream file = new FileInputStream(myConfig);
-            output = new FileOutputStream(myConfig, false);
-            prop.load(file);
-            prop.setProperty("footerContent", value);
-            prop.store(output, null);
-        } catch (IOException e) {
-            System.out.println("Erreur : " + e);
         }
     }
 }
